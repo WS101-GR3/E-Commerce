27f1a7cf1a92ce7537e76f89ada2c8a7fa7eea2b
WareBar <barryadam615@gmail.com>
2024-11-16 21:02:35 -0800

    registered Product and Review in admin panel

--- COMMERCE/PRODUCT/admin.py
+++ COMMERCE/PRODUCT/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Review
 
 # Register your models here.
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_id','product_category','product_name','product_price','product_rate')
+    order_by = ('-product_rate',)
+    search_fields = ('product_category','product_id','product_name')
+    list_filter = ('product_category','product_rate','isSoldOut')
 
-admin.site.register(Product)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('review_id','product_topic', 'review_context')
+    order_by = ('-review_id',)

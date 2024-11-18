ee8972b56ff52c5b2d066899e855126f68aef31c
WareBar <barryadam615@gmail.com>
2024-11-17 19:25:45 -0800

    modified for auto generate when media is uploaded

--- COMMERCE/COMMERCE/urls.py
+++ COMMERCE/COMMERCE/urls.py
@@ -15,10 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
+
 from . import views
 
 urlpatterns = [
+    # FOR MEDIA/FILE UPLOAD AND STORAGE
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
+
+
     path('product/', include('PRODUCT.urls')),
     path('', views.home_page, name='HOME'),
     path('admin/', admin.site.urls),

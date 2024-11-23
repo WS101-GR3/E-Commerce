64da95c8f14a0da4fee52017c6d9ed59c786b501
WareBar <barryadam615@gmail.com>
2024-11-22 19:35:56 -0800

    fields

--- COMMERCE/STORE/models.py
+++ COMMERCE/STORE/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class Store(models.Model):
     store_id = models.BigAutoField(null=False, primary_key=True)
     store_name = models.CharField(max_length=250, unique=True)
-    store_address = models.CharField(max_length=250)
-    store_owner = models.CharField(max_length=250)
-    store_email = models.CharField(max_length=250)
-    store_banner = models.ImageField()
-    store_description = models.TextField()
+    store_address = models.CharField(max_length=250, default=None)
+    store_owner = models.CharField(max_length=250, default=None)
+    store_email = models.CharField(max_length=250, default=None)
+    store_banner = models.ImageField(default='blank.jpg')
+    store_description = models.TextField(default="desc")
 
     created_at = models.DateTimeField(auto_now_add=True)
 

f39d13a034da882d306718d9e24bbfc78a1e2c51
WS101-GR3 <adam.abdulbarry.dll@gmail.com>
2024-11-24 21:37:44 +0800

    Add files via upload

---
+++ E-COMMERCE/E_COMMERCE/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve
+
+from . import views
+
+urlpatterns = [
+    # FOR MEDIA/FILE UPLOAD AND STORAGE
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
+
+
+    path('', views.home, name="HOME"),
+    path('product/', include('PRODUCT.urls')),
+    path('user/', include('USER.urls')),
+    path('admin/', admin.site.urls),
+]

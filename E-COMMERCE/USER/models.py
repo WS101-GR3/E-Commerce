c575e4db3f3f66b9f729a5c0470b84fea3a5bf16
WS101-GR3 <adam.abdulbarry.dll@gmail.com>
2024-11-24 21:41:37 +0800

    Add files via upload

---
+++ E-COMMERCE/USER/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PRODUCT.models import Laptop, PackageBundle
+
+
+
+class Basket(models.Model):
+    basket_id = models.BigAutoField(primary_key=True)
+    basket_owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='OWNER', null=True)
+    basket_items = models.ForeignKey('BasketItems', on_delete=models.DO_NOTHING, related_name='ITEMS', blank=True, null=True)
+
+
+
+
+    def __str__(self) -> str:
+        return f'{self.basket_owner}"s Basket'
+
+
+
+class BasketItems(models.Model):
+    baskteItem_id = models.BigAutoField(primary_key=True)
+    basket = models.ForeignKey(Basket, on_delete=models.CASCADE, related_name='BASKET_MAIN')
+    basket_laptop = models.ForeignKey(Laptop, on_delete=models.CASCADE, related_name='LAPTOP_ITEMS', blank=True)
+    basket_package = models.ForeignKey(PackageBundle, on_delete=models.CASCADE, related_name='PACKAGE_ITEMS', blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.basket}"s Item'
---
+++ E-COMMERCE/USER/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'USER'
+
+urlpatterns = [
+    path('login', views.login_user, name="LOGIN"),
+    path('register/', views.register_user, name="REGISTER"),
+    path('logout/', views.logout_user, name="LOGOUT")
+]

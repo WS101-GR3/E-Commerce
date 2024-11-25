05a4f3f98770f035f82c7dc8ce890dd735dd897b
WS101-GR3 <adam.abdulbarry.dll@gmail.com>
2024-11-25 13:47:11 +0000

    added cart PATH

--- E-COMMERCE/USER/urls.py
+++ E-COMMERCE/USER/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login', views.login_user, name="LOGIN"),
     path('register/', views.register_user, name="REGISTER"),
     path('logout/', views.logout_user, name="LOGOUT"),
-    path('cart/<str:userID>', views.getUserCart, name="FETCHCART")
+    path('cart/<str:userID>', views.getUserCart, name="FETCHCART"),
+    path('cart/add/<str:product_type>/<str:userID>/<str:product_id>', views.addToCart, name="ADD_TO_CART")
 ]

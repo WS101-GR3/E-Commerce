b9abdffb98f1056a5e69a82114256702484fddc1
WS101-GR3 <adam.abdulbarry.dll@gmail.com>
2024-11-25 02:12:06 +0000

    removed basketitem field in Basket Model

--- E-COMMERCE/USER/models.py
+++ E-COMMERCE/USER/models.py
@@ -6,22 +6,21 @@ from PRODUCT.models import Laptop, PackageBundle
 
 class Basket(models.Model):
     basket_id = models.BigAutoField(primary_key=True)
-    basket_owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='OWNER', null=True)
-    basket_items = models.ForeignKey('BasketItems', on_delete=models.DO_NOTHING, related_name='ITEMS', blank=True, null=True)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE, related_name='OWNER', null=True)
 
 
 
 
     def __str__(self) -> str:
-        return f"{self.basket_owner}'s Basket"
+        return f'{self.user_id}"s Basket'
 
 
 
 class BasketItems(models.Model):
     baskteItem_id = models.BigAutoField(primary_key=True)
     basket = models.ForeignKey(Basket, on_delete=models.CASCADE, related_name='BASKET_MAIN')
-    basket_laptop = models.ForeignKey(Laptop, on_delete=models.CASCADE, related_name='LAPTOP_ITEMS', blank=True)
-    basket_package = models.ForeignKey(PackageBundle, on_delete=models.CASCADE, related_name='PACKAGE_ITEMS', blank=True)
+    basket_laptop = models.ForeignKey(Laptop, on_delete=models.CASCADE, related_name='LAPTOP_ITEMS', blank=True, null=True)
+    basket_package = models.ForeignKey(PackageBundle, on_delete=models.CASCADE, related_name='PACKAGE_ITEMS', blank=True, null=True)
 
     def __str__(self) -> str:
         return f"{self.basket}'s Item"

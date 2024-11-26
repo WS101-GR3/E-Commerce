04f694eb35571cf76b494c0c869ec2ea404cb71a
WS101-GR3 <adam.abdulbarry.dll@gmail.com>
2024-11-26 13:50:42 +0000

    removed basketitems

--- E-COMMERCE/USER/models.py
+++ E-COMMERCE/USER/models.py
@@ -5,22 +5,10 @@ from PRODUCT.models import Laptop, PackageBundle
 
 
 class Basket(models.Model):
-    basket_id = models.BigAutoField(primary_key=True)
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE, related_name='OWNER', null=True)
+    user_id = models.PositiveIntegerField()
+    basket_item_id = models.PositiveIntegerField(default=0)
+    basket_item_name = models.CharField(max_length=350)
+    basket_item_price = models.PositiveBigIntegerField(default=0)
 
-
-
-
-    def __str__(self) -> str:
-        return f'{self.user_id}"s Basket'
-
-
-
-class BasketItems(models.Model):
-    baskteItem_id = models.BigAutoField(primary_key=True)
-    basket = models.ForeignKey(Basket, on_delete=models.CASCADE, related_name='BASKET_MAIN')
-    basket_laptop = models.ForeignKey(Laptop, on_delete=models.CASCADE, related_name='LAPTOP_ITEMS', blank=True, null=True)
-    basket_package = models.ForeignKey(PackageBundle, on_delete=models.CASCADE, related_name='PACKAGE_ITEMS', blank=True, null=True)
-
-    def __str__(self) -> str:
-        return f"{self.basket}'s Item"
+    def __str__(self):
+        return self.basket_item_name
